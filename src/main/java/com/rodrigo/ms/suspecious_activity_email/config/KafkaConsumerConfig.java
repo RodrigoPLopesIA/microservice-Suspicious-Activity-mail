2110b5e4a0e610d4fe6dc8d1fc1a09bfd37fc953
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-09-05 17:14:28 -0300

    feat: update Kafka consumer configuration to use MessageEventDTO and adjust listener topic

--- src/main/java/com/rodrigo/ms/suspecious_activity_email/config/KafkaConsumerConfig.java
+++ src/main/java/com/rodrigo/ms/suspecious_activity_email/config/KafkaConsumerConfig.java
@@ -2,29 +2,46 @@ package com.rodrigo.ms.suspecious_activity_email.config;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+import com.rodrigo.ms.suspecious_activity_email.dto.MessageEventDTO;
+
 @Configuration
+@EnableKafka
 public class KafkaConsumerConfig {
     
-
     @Bean
-    ConsumerFactory<String, Object> consumerFactory(){
-        Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "suspicious_activity_group");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        return new DefaultKafkaConsumerFactory<>(config);
+    public ConsumerFactory<String, MessageEventDTO> consumerFactory() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "suspicious_activity_group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        JsonDeserializer<MessageEventDTO> deserializer = new JsonDeserializer<>(MessageEventDTO.class);
+        deserializer.addTrustedPackages("*");
+
+
+        return new DefaultKafkaConsumerFactory<>(
+            props, 
+            new StringDeserializer(), 
+            deserializer);
     }
 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, MessageEventDTO> concurrentKafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, MessageEventDTO> factory = 
+        new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
--- src/main/java/com/rodrigo/ms/suspecious_activity_email/services/ConsumerService.java
+++ src/main/java/com/rodrigo/ms/suspecious_activity_email/services/ConsumerService.java
@@ -3,12 +3,14 @@ package com.rodrigo.ms.suspecious_activity_email.services;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.rodrigo.ms.suspecious_activity_email.dto.MessageEventDTO;
+
 @Service
 public class ConsumerService {
     
 
 
-    @KafkaListener(topics = "event_tickets", groupId = "suspicious_activity_group")
+    @KafkaListener(topics = "suspicious_activity_email_event", groupId = "suspicious_activity_group", containerFactory = "concurrentKafkaListenerContainerFactory")
     public void consumerMessage(MessageEventDTO message){
         System.out.println("consuming message!");
         System.out.println(message);
